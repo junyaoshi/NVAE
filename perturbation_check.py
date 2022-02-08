0becde331be15f3ce77bf5bc68204a265c6b0fbf
Junyao Shi <jackshi1999@gmail.com>
2022-02-08 15:53:56 -0500

    implemented robot mask perturbation

--- perturbation_check.py
+++ perturbation_check.py
@@ -5,10 +5,13 @@ import torch.distributed as dist
 import numpy as np
 import argparse
 from matplotlib import pyplot as plt
+from tqdm import tqdm
 
 import utils
 from model import AutoEncoder
 import datasets
+
+MASK_THRESHOLD = 0.91
 
 
 def main(eval_args):
@@ -18,17 +21,29 @@ def main(eval_args):
     checkpoint = torch.load(eval_args.checkpoint, map_location='cpu')
     args = checkpoint['args']
 
+    # check flags
     if not hasattr(args, 'ada_groups'):
         print('old model, no ada groups was found.')
         args.ada_groups = False
-
     if not hasattr(args, 'min_groups_per_scale'):
         print('old model, no min_groups_per_scale was found.')
         args.min_groups_per_scale = 1
-
     if not hasattr(args, 'num_mixture_dec'):
         print('old model, no num_mixture_dec was found.')
         args.num_mixture_dec = 10
+    perturb_type = None
+    if eval_args.perturb_state:
+        perturb_type = 'robot_state'
+        assert not eval_args.perturb_type and not eval_args.perturb_mask
+        assert args.cond_robot_state
+    if eval_args.perturb_type:
+        perturb_type = 'robot_type'
+        assert not eval_args.perturb_state and not eval_args.perturb_mask
+        assert args.cond_robot_type
+    if eval_args.perturb_mask:
+        perturb_type = 'robot_mask'
+        assert not eval_args.perturb_type and not eval_args.perturb_state
+        assert args.cond_robot_mask
 
     print(f'loaded the model at epoch {checkpoint["epoch"]}')
     arch_instance = utils.get_arch_cells(args.arch_instance)
@@ -53,80 +68,115 @@ def main(eval_args):
         valid_queue = train_queue
 
     # generate samples
-    with torch.no_grad():
-        data = next(iter(valid_queue))
-        x = data[0] if len(data) > 1 else data
-        x = x.cuda()
+    for i_sample in tqdm(range(eval_args.num_eval_samples), f'Generating {eval_args.num_eval_samples} samples...'):
+        with torch.no_grad():
+            data = next(iter(valid_queue))
+            x = data[0] if len(data) > 1 else data
+            x = x.cuda()
 
-        # change bit length
-        x = utils.pre_process(x, args.num_x_bits)
+            # change bit length
+            x = utils.pre_process(x, args.num_x_bits)
 
-        info = None
-        n = 1
-        assert args.cond_robot_state or args.cond_robot_type, "VAE must be conditional"
+            info = None
+            n = 1
+            assert args.cond_robot_state or args.cond_robot_type or args.cond_robot_mask, "VAE must be conditional"
 
-        # we need to feed the encoder info in this case
-        robot_state = data[1]
-        robot_type = data[3]
-        if args.cond_robot_state and args.cond_robot_type:
-            info = torch.cat((robot_state, robot_type), dim=1).cuda()
-        elif args.cond_robot_state:
-            info = robot_state.cuda()
-        elif args.cond_robot_type:
-            info = robot_type.cuda()
-
-        logits, log_q, log_p, kl_all, kl_diag = model(x, info)
-        output = model.decoder_output(logits)
-        output_img = output.mean if isinstance(output,
-                                               torch.distributions.bernoulli.Bernoulli) else output.sample()
-        x_tiled = utils.tile_image(x, n)
-        output_tiled = utils.tile_image(output_img, n)
-
-        # perturb image
-        if eval_args.perturb_state:
-            assert args.cond_robot_state
-            perturbed_robot_pos = 2 * torch.rand(2) - 1
-            perturbed_robot_state = torch.clone(robot_state)
-            perturbed_robot_state[0, :2] = perturbed_robot_pos
+            # we need to feed the encoder info in this case
+            robot_state = data[1]
+            robot_type = data[3]
+            if args.cond_robot_mask:
+                x_clone = torch.clone(x)
+                mask = x_clone[:, 0] < MASK_THRESHOLD
+                info = mask.float().unsqueeze(1).cuda()
             if args.cond_robot_state and args.cond_robot_type:
-                robot_type = data[3]
-                info = torch.cat((perturbed_robot_state, robot_type), dim=1).cuda()
+                info = torch.cat((robot_state, robot_type), dim=1).cuda()
             elif args.cond_robot_state:
-                info = perturbed_robot_state.cuda()
-        else:
-            assert args.cond_robot_type
-            num_classes = 4
-            perturbed_robot_type = F.one_hot(torch.tensor([np.random.randint(num_classes)]),
-                                             num_classes=num_classes)
-            if args.cond_robot_state and args.cond_robot_type:
-                robot_state = data[1]
-                info = torch.cat((robot_state, perturbed_robot_type), dim=1).cuda()
+                info = robot_state.cuda()
             elif args.cond_robot_type:
-                info = perturbed_robot_type.cuda()
-        perturbed_logits, log_q, log_p, kl_all, kl_diag = model(x, info)
-        perturbed_output = model.decoder_output(perturbed_logits)
-        perturbed_output_img = output.mean if isinstance(perturbed_output,
-                                               torch.distributions.bernoulli.Bernoulli) else perturbed_output.sample()
-        perturbed_output_tiled = utils.tile_image(perturbed_output_img, n)
+                info = robot_type.cuda()
 
+            logits, log_q, log_p, kl_all, kl_diag = model(x, info)
+            output = model.decoder_output(logits)
+            output_img = output.mean if isinstance(output,
+                                                   torch.distributions.bernoulli.Bernoulli) else output.sample()
+            x_tiled = utils.tile_image(x, n)
+            output_tiled = utils.tile_image(output_img, n)
 
-        fig, axs = plt.subplots(3, 1, figsize=(5, 15))
-        perturb_type = 'robot_state' if eval_args.perturb_state else 'robot_type'
-        original_value = [round(v.item(), 4) for v in robot_state[0, :2]] \
-            if eval_args.perturb_state else robot_type.argmax().item()
-        perturbed_value = [round(v.item(), 4) for v in perturbed_robot_state[0, :2]] \
-            if eval_args.perturb_state else perturbed_robot_type.argmax().item()
-        fig.suptitle(f'{perturb_type} perturbation sanity check')
-        axs[0].imshow(x_tiled.permute(1, 2, 0).cpu().data.numpy())
-        axs[0].set_title(f'Input | {perturb_type}: {original_value}')
-        axs[1].imshow(output_tiled.permute(1, 2, 0).cpu().data.numpy())
-        axs[1].set_title(f'Output | {perturb_type}: {original_value}')
-        axs[2].imshow(perturbed_output_tiled.permute(1, 2, 0).cpu().data.numpy())
-        axs[2].set_title(f'Perturbed Output | {perturb_type}: {perturbed_value}')
-        plt.tight_layout()
-        plt.savefig(f'plots/{perturb_type}_perturb_check_2.png')
-        plt.show()
-        plt.close()
+            # perturb image
+            if eval_args.perturb_state:
+                perturbed_robot_pos = 2 * torch.rand(2) - 1
+                perturbed_robot_state = torch.clone(robot_state)
+                perturbed_robot_state[0, :2] = perturbed_robot_pos
+                if args.cond_robot_state and args.cond_robot_type:
+                    robot_type = data[3]
+                    info = torch.cat((perturbed_robot_state, robot_type), dim=1).cuda()
+                elif args.cond_robot_state:
+                    info = perturbed_robot_state.cuda()
+            elif eval_args.perturb_type:
+                num_classes = 4
+                perturbed_robot_type = F.one_hot(torch.tensor([np.random.randint(num_classes)]),
+                                                 num_classes=num_classes)
+                if args.cond_robot_state and args.cond_robot_type:
+                    robot_state = data[1]
+                    info = torch.cat((robot_state, perturbed_robot_type), dim=1).cuda()
+                elif args.cond_robot_type:
+                    info = perturbed_robot_type.cuda()
+            elif eval_args.perturb_mask:
+                perturbed_data = next(iter(valid_queue))
+                perturbed_x = perturbed_data[0] if len(perturbed_data) > 1 else perturbed_data
+                perturbed_x = perturbed_x.cuda()
+                perturbed_mask = perturbed_x[:, 0] < MASK_THRESHOLD
+                info = perturbed_mask.float().unsqueeze(1).cuda()
+            else:
+                raise NotImplementedError
+
+            perturbed_logits, log_q, log_p, kl_all, kl_diag = model(x, info)
+            perturbed_output = model.decoder_output(perturbed_logits)
+            perturbed_output_img = output.mean if isinstance(perturbed_output,
+                                                   torch.distributions.bernoulli.Bernoulli) else perturbed_output.sample()
+            perturbed_output_tiled = utils.tile_image(perturbed_output_img, n)
+
+            if eval_args.perturb_state or eval_args.perturb_type:
+                fig, axs = plt.subplots(3, 1, figsize=(5, 15))
+                original_value = [round(v.item(), 4) for v in robot_state[0, :2]] \
+                    if eval_args.perturb_state else robot_type.argmax().item()
+                perturbed_value = [round(v.item(), 4) for v in perturbed_robot_state[0, :2]] \
+                    if eval_args.perturb_state else perturbed_robot_type.argmax().item()
+                fig.suptitle(f'{perturb_type} perturbation sanity check')
+                axs[0].imshow(x_tiled.permute(1, 2, 0).cpu().data.numpy())
+                axs[0].set_title(f'Input | {perturb_type}: {original_value}')
+                axs[1].imshow(output_tiled.permute(1, 2, 0).cpu().data.numpy())
+                axs[1].set_title(f'Output | {perturb_type}: {original_value}')
+                axs[2].imshow(perturbed_output_tiled.permute(1, 2, 0).cpu().data.numpy())
+                axs[2].set_title(f'Perturbed Output | {perturb_type}: {perturbed_value}')
+                plt.tight_layout()
+                plt.savefig(f'plots/{perturb_type}_perturb_check_2.png')
+                plt.show()
+                plt.close()
+            else:
+                fig, axs = plt.subplots(3, 2, figsize=(10, 15))
+                fig.suptitle(f'{perturb_type} perturbation sanity check')
+
+                # original
+                axs[0, 0].imshow(x_tiled.permute(1, 2, 0).cpu().data.numpy())
+                axs[0, 0].set_title(f'Original Input')
+                axs[1, 0].imshow(mask.float().permute(1, 2, 0).cpu().data.numpy())
+                axs[1, 0].set_title(f'Original Conditional Mask')
+                axs[2, 0].imshow(output_tiled.permute(1, 2, 0).cpu().data.numpy())
+                axs[2, 0].set_title(f'Original Output')
+
+                # perturb
+                axs[0, 1].imshow(x_tiled.permute(1, 2, 0).cpu().data.numpy())
+                axs[0, 1].set_title(f'Original Input')
+                axs[1, 1].imshow(perturbed_mask.float().permute(1, 2, 0).cpu().data.numpy())
+                axs[1, 1].set_title(f'Perturbed Conditional Mask')
+                axs[2, 1].imshow(perturbed_output_tiled.permute(1, 2, 0).cpu().data.numpy())
+                axs[2, 1].set_title(f'Perturbed Output')
+
+                plt.tight_layout()
+                plt.savefig(f'plots/{perturb_type}_beta=4/perturb_check_{i_sample + 1}.png')
+                plt.show()
+                plt.close()
 
 
 def init_processes(rank, size, fn, args, port='6020'):
@@ -152,6 +202,8 @@ if __name__ == '__main__':
                         help='location of the data corpus')
     parser.add_argument('--eval_on_train', action='store_true', default=False,
                         help='Settings this to true will evaluate the model on training data.')
+    parser.add_argument('--num_eval_samples', type=int, default=5,
+                        help='number of evaluation samples to generate')
 
     parser.add_argument('--perturb_state', action='store_true', default=False,
                         help='If true, perturb robot state in the conditional input.')
